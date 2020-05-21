f21cdbf12429106fba8571f623092ef91413ed6e
Simon Krughoff <krughoff@lsst.org>
2020-05-21 21:32:29 +0000

    Factor measurement into a subtask

--- tasks/MatchedCatalogsAnalysis.py
+++ tasks/MatchedCatalogsAnalysis.py
@@ -1,17 +1,13 @@
-import astropy.units as u
-import numpy as np
 import lsst.pipe.base as pipeBase
-from lsst.verify import Measurement
 import lsst.pex.config as pexConfig
-from sst_metrics_utils.filtermatches import filterMatches
 from lsst.verify.tasks import MetricTask, MetricConfig, MetricConnections
-from lsst.validate.drp.repeatability import calcPhotRepeat
+from MeasureTasks import measure_task_map
 
 # The first thing to do is to define a Connections class. This will define all
 # the inputs and outputs that our task requires
 
 
-class MatchedCatalogTaskConnections(MetricConnections,
+class MatchedCatalogAnalysisTaskConnections(MetricConnections,
                                     dimensions=("tract", "patch", "abstract_filter",
                                                 "instrument", "skymap")):
     matchedCatalog = pipeBase.connectionTypes.Input(doc="Input matched catalog.",
@@ -26,50 +22,21 @@ class MatchedCatalogTaskConnections(MetricConnections,
                                                   name="metricvalue_{package}_{metric}")
 
 
-class MatchedCatalogTaskConfig(MetricConfig,
-                               pipelineConnections=MatchedCatalogTaskConnections):
-    pass
+class MatchedCatalogAnalysisTaskConfig(MetricConfig,
+                               defaultTemplates={"metric": None},
+                               pipelineConnections=MatchedCatalogAnalysisTaskConnections):
+    measure = pexConfig.ConfigurableField(
+        target=measure_task_map["nsrcMeas"],
+        doc="Measure task")
 
 
-class NumSourcesTask(MetricTask):
+class MatchedCatalogAnalysisTask(MetricTask):
 
-    ConfigClass = MatchedCatalogTaskConfig
-    _DefaultName = "numSourcesTask"
-
-    def run(self, matchedCatalog):
-        self.log.info(f"Counting sources in matched catalog")
-        nSources = len(matchedCatalog)
-        meas = Measurement("nsrcMeas", nSources * u.count)
-        return pipeBase.Struct(measurement=meas)
-
-
-class MeasurePA1TaskConfig(MatchedCatalogTaskConfig):
-    brightSnrMin = pexConfig.Field(doc="Minimum median SNR for a source to be considered bright.",
-                                   dtype=float, default=50)
-    brightSnrMax = pexConfig.Field(doc="Maximum median SNR for a source to be considered bright.",
-                                   dtype=float, default=np.Inf)
-
-
-class MeasurePA1Task(MetricTask):
-
-    ConfigClass = MeasurePA1TaskConfig
-    _DefaultName = "measurePA1Task"
-
-    def __init__(self, config: MeasurePA1TaskConfig, *args, **kwargs):
+    ConfigClass = MatchedCatalogAnalysisTaskConfig
+    _DefaultName = "matchedCatalogAnalysisTask"
+    def __init__(self, config, *args, **kwargs):
         super().__init__(*args, config=config, **kwargs)
-        self.brightSnrMin = self.config.brightSnrMin
-        self.brightSnrMax = self.config.brightSnrMax
+        self.makeSubtask('measure')
 
     def run(self, matchedCatalog):
-        self.log.info(f"Measuring PA1")
-
-        filteredCat = filterMatches(matchedCatalog) #, extended=False, isPrimary=False)
-        magKey = filteredCat.schema.find('slot_PsfFlux_mag').key
-
-        # Require at least nMinPA1=10 objects to calculate the repeatability:
-        nMinPA1 = 50
-        if filteredCat.count > nMinPA1:
-            pa1 = calcPhotRepeat(filteredCat, magKey)
-            return pipeBase.Struct(measurement=Measurement("PA1", pa1['repeatability']))
-        else:
-            return pipeBase.Struct(measurement=Measurement("PA1", np.nan*u.mmag))
+        return self.measure.run(matchedCatalog)
