469fc8647c26508267a18bb1960b451fff6bdbc4
Simon Krughoff <krughoff@lsst.org>
2020-05-22 22:52:57 +0000

    Add measurement of TEx

--- tasks/MeasureTasks.py
+++ tasks/MeasureTasks.py
@@ -6,6 +6,8 @@ from lsst.verify import Measurement, ThresholdSpecification
 import lsst.pex.config as pexConfig
 from sst_metrics_utils.filtermatches import filterMatches
 from lsst.validate.drp.repeatability import calcPhotRepeat
+from lsst.validate.drp.calcsrd.tex import (correlation_function_ellipticity_from_matches,
+                                           select_bin_from_corr)
 
 class NumSourcesTask(Task):
 
@@ -49,3 +51,28 @@ class PA1Task(Task):
             return Struct(measurement=Measurement("PA1", pa1['repeatability']))
         else:
             return Struct(measurement=Measurement("PA1", np.nan*u.mmag))
+
+
+class TExTaskConfig(Config):
+    annulus_r = Field(doc="Radial size of the annulus in arcmin",
+                      dtype=float, default=1.)
+    comparison_operator = Field(doc="String representation of the operator to use in comparisons",
+                                dtype=str, default="<=")
+
+class TExTask(Task):
+    ConfigClass = TExTaskConfig
+    _DefaultName = "TExTask"
+
+    def run(self, matchedCatalog, metric_name):
+        self.log.info(f"Measuring {metric_name}")
+
+        D = self.config.annulus_r * u.arcmin
+        filteredCat = filterMatches(matchedCatalog)
+        nMinTEx = 50
+        if filteredCat.count <= nMinTEx:
+            return Struct(measurement=Measurement(metric_name, np.nan*u.Unit('')))
+
+        radius, xip, xip_err = correlation_function_ellipticity_from_matches(filteredCat)
+        operator = ThresholdSpecification.convert_operator_str(self.config.comparison_operator)
+        corr, corr_err = select_bin_from_corr(radius, xip, xip_err, radius=D, operator=operator)
+        return Struct(measurement=Measurement(metric_name, np.abs(corr)*u.Unit('')))
