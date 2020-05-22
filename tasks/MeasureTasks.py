6a1c2b7362a266f96493cc8cb75708e06c375fcf
Simon Krughoff <krughoff@lsst.org>
2020-05-22 23:17:16 +0000

    Add AMx measurements

--- tasks/MeasureTasks.py
+++ tasks/MeasureTasks.py
@@ -8,6 +8,7 @@ from sst_metrics_utils.filtermatches import filterMatches
 from lsst.validate.drp.repeatability import calcPhotRepeat
 from lsst.validate.drp.calcsrd.tex import (correlation_function_ellipticity_from_matches,
                                            select_bin_from_corr)
+from lsst.validate.drp.calcsrd.amx import calcRmsDistances
 
 class NumSourcesTask(Task):
 
@@ -76,3 +77,39 @@ class TExTask(Task):
         operator = ThresholdSpecification.convert_operator_str(self.config.comparison_operator)
         corr, corr_err = select_bin_from_corr(radius, xip, xip_err, radius=D, operator=operator)
         return Struct(measurement=Measurement(metric_name, np.abs(corr)*u.Unit('')))
+
+
+class AMxTaskConfig(Config):
+    annulus_r = Field(doc="Radial distance of the annulus in arcmin",
+                      dtype=float, default=5.)
+    width = Field(doc="Width of annulus in arcmin",
+                  dtype=float, default=2.)
+    bright_mag_cut = Field(doc="Bright limit of catalog entries to include",
+                           dtype=float, default=17.0)
+    faint_mag_cut = Field(doc="Faint limit of catalog entries to include",
+                           dtype=float, default=21.5)
+
+
+class AMxTask(Task):
+    ConfigClass = AMxTaskConfig
+    _DefaultName = "AMxTask"
+
+    def run(self, matchedCatalog, metric_name):
+        self.log.info(f"Measuring {metric_name}")
+
+        filteredCat = filterMatches(matchedCatalog)
+
+        magRange = np.array([self.config.bright_mag_cut, self.config.faint_mag_cut]) * u.mag
+        D = self.config.annulus_r * u.arcmin
+        width = self.config.width * u.arcmin
+        annulus = D + (width/2)*np.array([-1, +1])
+
+        rmsDistances = calcRmsDistances(
+            filteredCat,
+            annulus,
+            magRange=magRange)
+
+        if len(rmsDistances) == 0:
+            return Struct(measurement=Measurement(metric_name, np.nan*u.marcsec))
+
+        return Struct(measurement=Measurement(metric_name, np.median(rmsDistances.to(u.marcsec))))
