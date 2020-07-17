e75c4f0e7f7ae0b52f9535860fce3168b1e1a98e
Simon Krughoff <krughoff@lsst.org>
2020-07-17 18:20:16 +0000

    Rename to match test class name

--- tests/testAMx.py
+++ tests/testAMx.py
@@ -19,7 +19,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-"""Unit tests for the metrics measurement system: AM1.
+"""Unit tests for the metrics measurement system: AMx.
 """
 
 import unittest
