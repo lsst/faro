8094aad4ee4cff600691f3b202deed88002fd86d
Simon Krughoff <krughoff@lsst.org>
2021-02-11 16:16:17 -0700

    Switch to faro

--- bin/measureHscMetrics.sh
+++ bin/measureHscMetrics.sh
@@ -4,6 +4,6 @@ set -e
 
 sh hsc_gen2_to_gen3.sh
 
-pipetask run -j $NUMPROC -b validation_hsc_gen3/butler.yaml --register-dataset-types -p $METRIC_PIPELINE_TASKS_DIR/pipelines/validate_drp_metrics_pipeline.yaml -d "" -o jenkins/validate_drp_metrics_all -i shared/valid_hsc_all,skymaps 
+pipetask run -j $NUMPROC -b validation_hsc_gen3/butler.yaml --register-dataset-types -p $FARO_DIR/pipelines/validate_drp_metrics_pipeline.yaml -d "" -o jenkins/validate_drp_metrics_all -i shared/valid_hsc_all,skymaps 
 
 make_job_document.py validation_hsc_gen3 jenkins/validate_drp_metrics_all
