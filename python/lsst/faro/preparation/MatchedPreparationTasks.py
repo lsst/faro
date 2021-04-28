3993c73bda95474ad1f47b4c46c8934ac752f726
Jeff Carlin <jcarlin@lsst.org>
2021-04-27 20:45:28 -0500

    Fix a bug in applying jointcal.

--- python/lsst/faro/preparation/MatchedPreparationTasks.py
+++ python/lsst/faro/preparation/MatchedPreparationTasks.py
@@ -47,14 +47,15 @@ class TractMatchedPreparationTaskConnections(MatchedBaseTaskConnections,
 
 class TractMatchedPreparationTaskConfig(MatchedBaseTaskConfig,
                                         pipelineConnections=TractMatchedPreparationTaskConnections):
-    doApplyExternalSkyWcs = pexConfig.Field(doc="Whether or not to use the external wcs.",
-                                            dtype=bool, default=False)
-    useGlobalExternalSkyWcs = pexConfig.Field(doc="Whether or not to use the global external wcs.",
-                                              dtype=bool, default=False)
-    doApplyExternalPhotoCalib = pexConfig.Field(doc="Whether or not to use the external photoCalib.",
-                                                dtype=bool, default=False)
-    useGlobalExternalPhotoCalib = pexConfig.Field(doc="Whether or not to use the global external photoCalib.",
-                                                  dtype=bool, default=False)
+#    doApplyExternalSkyWcs = pexConfig.Field(doc="Whether or not to use the external wcs.",
+#                                            dtype=bool, default=False)
+#    useGlobalExternalSkyWcs = pexConfig.Field(doc="Whether or not to use the global external wcs.",
+#                                              dtype=bool, default=False)
+#    doApplyExternalPhotoCalib = pexConfig.Field(doc="Whether or not to use the external photoCalib.",
+#                                                dtype=bool, default=False)
+#    useGlobalExternalPhotoCalib = pexConfig.Field(doc="Whether or not to use the global external photoCalib.",
+#                                                  dtype=bool, default=False)
+    pass
 
 
 class TractMatchedPreparationTask(MatchedTractBaseTask):
