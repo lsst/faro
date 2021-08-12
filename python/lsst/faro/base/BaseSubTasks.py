b03c468f83c6f7c5b8282f6c8b46a9b754d35367
edennihy <edennihy@lsst.org>
2021-08-12 17:29:11 +0000

    Add COPYRIGHT file to base directory, remove whitespace after preamble in python/lsst/faro/base/BaseSubTasks.py

--- python/lsst/faro/base/BaseSubTasks.py
+++ python/lsst/faro/base/BaseSubTasks.py
@@ -19,7 +19,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-
 import astropy.units as u
 import numpy as np
 from lsst.pipe.base import Struct, Task
